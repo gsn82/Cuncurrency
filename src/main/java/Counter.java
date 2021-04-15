2cc04ff910c182040030de7644f9398264f3a192
Gorshkov Sergey <Gsn82@mail.ru>
2021-04-15 12:27:48 +0500

    83

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,16 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
 
-    private int value;
+    private AtomicInteger value = new AtomicInteger();
 
-    public void inc(){
-        value++;
+    public void inc() {
+        value.getAndIncrement();
     }
 
-    public void dec(){
-        value--;
+    public void dec() {
+        value.getAndDecrement();
     }
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 }
